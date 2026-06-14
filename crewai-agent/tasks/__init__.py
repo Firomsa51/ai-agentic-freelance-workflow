7adc4b1ff4e819894545a598c3a6abdfeaa54e86
FiromsaDev <149601400+Firomsa51@users.noreply.github.com>
2026-06-14 18:42:30 +0300

    Update __init__.py

--- crewai-agent/tasks/__init__.py
+++ crewai-agent/tasks/__init__.py
@@ -1,5 +1,11 @@
 from .scout_task import build_scout_task
 from .analyst_task import build_analyst_task
+from .strategist_task import build_strategist_task
 from .proposal_task import build_proposal_task
 
-__all__ = ["build_scout_task", "build_analyst_task", "build_proposal_task"]
+__all__ = [
+    "build_scout_task",
+    "build_analyst_task",
+    "build_strategist_task",
+    "build_proposal_task",
+]
